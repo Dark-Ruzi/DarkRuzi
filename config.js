0c2d295cda301d1db48d87c93636b838ada50ce0
Dark-Ruzi <pasindusbandara923@gmail.com>
2025-02-10 19:05:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,6 @@ module.exports = {
   MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
   OWNER_NUM: process.env.OWNER_NUM || "94765718443",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/140577992?v=4.jpg",
-  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥶Made By Bandara🥶",
- 
+  MODE: process.env.MODE || "public"
 
 };
